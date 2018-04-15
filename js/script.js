6642469e4c410f5ea3bfe0fb20e86ffc47a16bb8
Christian <christiantisby1@gmail.com>
2018-04-15 17:29:11 -0400

    final commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+$(window).on("load", function() {
+  
+  $(".loader .inner").fadeOut(500, function() {
+      $(".loader"),fadeOut(750);
+  });
+
+});
+
 $(document).ready(function() {
 
   $('#slides').superslides({
@@ -105,6 +113,35 @@ $(document).ready(function() {
       return false;
 
    });
+
+   $("#navigation li a").click(function(e){
+      e.preventDefault();
+
+      var targetElement = $(this).attr("href");
+      var targetPosition = $(targetElement).offset().top;
+      $("html, body").animate({scrollTop: targetPosition - 50}, "slow")
+
+   });
+
+   const nav = $("#navigation");
+   const navTop = nav.offset().top;
+
+   $(window).on("scroll", stickyNavigation);
+
+   function stickyNavigation() {
+      
+    var body = $("body");
+
+    if ($(window).scrollTop() >= navTop) {
+      body.css("padding-top", nav.outerHeight() + "px");
+      body.addClass("fixedNav");
+    }
+    else {
+      body.css("padding-top", 0);
+      body.removeClass("fixedNav");
+    }
+
+   }
 
 });
 
